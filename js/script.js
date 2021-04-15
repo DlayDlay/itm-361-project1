430019d14d12f5d1651ebcaba0e495bc2468c5f8
DlayDlay <58670262+DlayDlay@users.noreply.github.com>
2021-04-14 22:00:50 -0500

    basic slider added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 
 
-function initMap() {
+/*function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
 }
 
-window.addEventListener("load", initMap); 
+window.addEventListener("load", initMap);*/
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
+
+ 
+
