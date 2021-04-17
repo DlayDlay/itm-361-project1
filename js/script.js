e563c8ace5b082dbc756060436a7530e79a40883
DlayDlay <58670262+DlayDlay@users.noreply.github.com>
2021-04-17 15:45:06 -0500

    new attempts at getting map working, box slider

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 
 
-/*function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+function initMap() {
+  const myLatLng = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatLng,
+  });
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Home Sweet Home!",
   });
 }
 
-window.addEventListener("load", initMap);*/
+
+
+
+
+window.addEventListener("load", initMap);
+
+
 
 $(document).ready(function(){
   $('.bxslider').bxSlider();
